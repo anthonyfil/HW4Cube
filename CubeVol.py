fd52c6ff562018a2ff2578182ffba3036e207b09
Anthony <filippea@oregonstate.edu>
2021-02-06 16:13:32 -0800

    Updated tests and file
    fixed for floats

--- CubeVol.py
+++ CubeVol.py
@@ -1,5 +1,5 @@
 def Volume(x):
-	if(type(x) == type(1) and x >= 0):
+	if((type(x) == type(1) or type(x) == type(1.1)) and x >= 0):
 		return (x*x*x)
 	else:
 		return -1;
--- test_CubeVol.py
+++ test_CubeVol.py
@@ -11,18 +11,21 @@ class test_CubeVol(unittest.TestCase):
 		
 		result = CubeVol.Volume(3)
 		self.assertEqual(result, 27)
+	def test_volume_negatives(self):
                 #Testing negatives
 		result = CubeVol.Volume(-1)
 		self.assertEqual(result, -1)
 		
 		result = CubeVol.Volume(-5)
 		self.assertEqual(result, -1)
+	def test_volume_text(self):
 		#Testing text
 		result = CubeVol.Volume("Words")
 		self.assertEqual(result, -1)
 		
 		result = CubeVol.Volume("3")
 		self.assertEqual(result, -1)
+	def test_volume_complex(self):
 		#testing complex numbers
 		result = CubeVol.Volume(complex(5,2))
 		self.assertEqual(result, -1)
